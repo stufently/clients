2cc5ab206075a2d28b465321acfdf48be55805bc
Nick Krantz <nick@livefront.com>
2026-03-20 12:51:16 -0500

    remove unused block size

--- libs/common/src/platform/enums/azure-block-size.enum.ts
+++
@@ -1,8 +0,0 @@
-/** Commonly used block sizes for Azure uploads */
-export const AzureUploadBlockSize = {
-  32: 33_554_432, // 32 MiB
-  128: 134_217_728, // 128 MiB
-  4000: 4_194_304_000, // 4000 MiB, the maximum block size allowed by Azure
-} as const;
-
-export type AzureUploadBlockSize = (typeof AzureUploadBlockSize)[keyof typeof AzureUploadBlockSize];
