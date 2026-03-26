5f165df4a5b53d278f32ef059de3989690b2470b
adudek-bw <adudek@bitwarden.com>
2026-03-26 17:07:59 -0400

    require name field, change expiration day to select

--- apps/web/src/app/tools/receive/receive-add-edit.component.ts
+++ apps/web/src/app/tools/receive/receive-add-edit.component.ts
@@ -1,15 +1,41 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
-import { ButtonModule, DialogModule, FormFieldModule } from "@bitwarden/components";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { ButtonModule, DialogModule, FormFieldModule, SelectModule } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
 
 @Component({
   templateUrl: "./receive-add-edit.component.html",
-  imports: [FormsModule, DialogModule, FormFieldModule, ButtonModule, I18nPipe],
+  imports: [
+    ReactiveFormsModule,
+    DialogModule,
+    FormFieldModule,
+    SelectModule,
+    ButtonModule,
+    I18nPipe,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReceiveAddEditComponent {
-  protected readonly name = "";
-  protected readonly expirationDays = 7;
+  protected readonly expirationDayOptions: { label: string; value: number }[];
+
+  protected readonly form = this.formBuilder.group({
+    name: ["", Validators.required],
+    expirationDays: [7, Validators.required],
+  });
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly i18nService: I18nService,
+  ) {
+    this.expirationDayOptions = [
+      { label: this.i18nService.t("oneDay"), value: 1 },
+      { label: this.i18nService.t("days", "2"), value: 2 },
+      { label: this.i18nService.t("days", "3"), value: 3 },
+      { label: this.i18nService.t("days", "7"), value: 7 },
+      { label: this.i18nService.t("days", "14"), value: 14 },
+      { label: this.i18nService.t("days", "30"), value: 30 },
+    ];
+  }
 }
