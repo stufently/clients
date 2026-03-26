3dabc5b86954ee57778c920b00dd2ed4d7110e86
Leslie Xiong <lxiong@livefront.com>
2026-03-26 14:19:05 -0400

    fixed lint errors

--- libs/components/src/menu/filter-menu-header.component.ts
+++ libs/components/src/menu/filter-menu-header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 
-import { IconButtonModule , BitIconButtonComponent } from "../icon-button";
+import { I18nPipe } from "@bitwarden/ui-common";
+
+import { IconButtonModule, BitIconButtonComponent } from "../icon-button";
 
 // FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
 // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
   selector: "bit-filter-menu-header",
   templateUrl: "filter-menu-header.component.html",
-  imports: [IconButtonModule, BitIconButtonComponent],
+  imports: [IconButtonModule, BitIconButtonComponent, I18nPipe],
 })
 export class FilterMenuHeaderComponent {}
